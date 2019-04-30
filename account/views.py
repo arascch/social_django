390ab6de1e3f730c6aed88879439ae687c2ac058
arasch <arash.ask.94@gmail.com>
2019-04-30 23:05:00 +0430

    commit

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate , login
 from .forms import LoginForm , UserRegisterationForm , UserEditForm , ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+from django.contrib import messages
 
 
 
@@ -55,10 +56,13 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request , 'Profile updated successfully')
+        else:
+            messages.error(request , 'Error updating your profile')
     else:
         user_form = UserEditForm(instance = request.user)
         profile_form = ProfileEditForm(instance = request.user.profile)
     
     return render(request , 'account/edit.html' ,{'user_form' : user_form ,  'profile_form' : profile_form})
 
-        
+    
